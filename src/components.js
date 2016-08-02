a45309b7ca7eac1ec57c9ec95161f47150104266
Wickramaranga <umstek@live.com>
2016-08-02 22:38:48 +0530

    Component improvement.

--- src/components.js
+++ src/components.js
@@ -1,9 +1,9 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
 class BaseComponent extends React.Component { // Base Class for ease. :)
     constructor(props) {
         super(props);
-        let state = {};
-        Object.assign(state, props);
-        this.state = state;
     }
 }
 
@@ -25,10 +25,12 @@ class NullValueComponent extends BaseComponent {
 class BooleanValueComponent extends BaseComponent {
     constructor(props) {
         super(props);
+        this.state = {value: props.value};
+        this._handleChange = this._handleChange.bind(this);
     }
 
     render() {
-        return <span className="switch booleanValueComponent" onChange={this._handleChange.bind(this)}>
+        return <span className="switch booleanValueComponent" onChange={this._handleChange}>
             <label>
                 False
                 <input type="checkbox" value={this.state.value}/>
@@ -38,25 +40,28 @@ class BooleanValueComponent extends BaseComponent {
         </span>;
     }
 
-    _handleChange(e) {
-        let value = e.target.value == "on";
+    _handleChange(evt) {
+        let value = evt.target.value == "on"; // Convert 'on' and 'off' to true and false.
         this.setState({value});
-        // Feedback function enables propagation of data to upper layers.
+        // Feedback function enables propagation of data to parent components.
         this.props.feedback({value});
     }
 }
+BooleanValueComponent.propTypes = {value: React.PropTypes.bool, feedback: React.PropTypes.func};
+BooleanValueComponent.defaultProps = {value: false, feedback: (obj) => console.log(obj.value)};
 
 
 class NumberValueComponent extends BaseComponent {
     constructor(props) {
         super(props);
+        this.state = {value: props.value};
+        this._handleChange = this._handleChange.bind(this);
     }
 
     render() {
         return <span className="numberValueComponent">
             <label>
-                <input className="input-field" type="number" value={this.state.value}
-                       onChange={this._handleChange.bind(this)}/>
+                <input className="input-field" type="number" value={this.state.value} onChange={this._handleChange}/>
             </label>
         </span>
     }
@@ -67,18 +72,21 @@ class NumberValueComponent extends BaseComponent {
         this.props.feedback({value});
     }
 }
+NumberValueComponent.propTypes = {value: React.PropTypes.number, feedback: React.PropTypes.func};
+NumberValueComponent.defaultProps = {value: 0, feedback: (obj) => console.log(obj.value)};
 
 
 class TextValueComponent extends BaseComponent {
     constructor(props) {
         super(props);
+        this.state = {value: props.value};
+        this._handleChange = this._handleChange.bind(this);
     }
 
     render() {
         return <span className="textValueComponent">
             <label>
-                <input className="input-field" type="text" value={this.state.value}
-                       onChange={this._handleChange.bind(this)}/>
+                <input className="input-field" type="text" value={this.state.value} onChange={this._handleChange}/>
             </label>
         </span>
     }
@@ -89,98 +97,208 @@ class TextValueComponent extends BaseComponent {
         this.props.feedback({value});
     }
 }
+TextValueComponent.propTypes = {value: React.PropTypes.string, feedback: React.PropTypes.func};
+TextValueComponent.defaultProps = {value: 0, feedback: (obj) => console.log(obj.value)};
 
 
 class CombinedValueComponent extends BaseComponent {
     constructor(props) {
         super(props);
+        this.state = {value: props.value, type: props.type};
+        this._handleComponentClick = this._handleComponentClick.bind(this);
+        this._handleComponentValueChange = this._handleComponentValueChange.bind(this);
     }
 
     render() {
         return <div className="combinedValueComponent">
             <ul className="collapsible" data-collapsible="accordion">
+
                 <div className="container"><h5>Atom Types</h5></div>
-                <li name="null" onClick={this._handleItemClick.bind(this)}>
+
+                <li name="null" onClick={this._handleComponentClick}>
                     <div className={"collapsible-header" + (this.state.type == "null" ? " active" : "")}>
                         <i className="material-icons">not_interested</i>Null
                     </div>
-                    <div className="collapsible-body"><p><NullValueComponent /></p></div>
+                    <div className="collapsible-body">
+                        <p>
+                            <NullValueComponent />
+                        </p>
+                    </div>
                 </li>
-                <li name="boolean" onClick={this._handleItemClick.bind(this)}>
+
+                <li name="boolean" onClick={this._handleComponentClick}>
                     <div className={"collapsible-header"+ (this.state.type == "boolean" ? " active" : "")}>
                         <i className="material-icons">thumbs_up_down</i>Boolean
                     </div>
-                    <div className="collapsible-body"><p><BooleanValueComponent
-                        value={this.state.type == "boolean" ? this.state.value : false}
-                        feedback={this._feedback.bind(this)}/>
-                    </p></div>
-                </li>
-                <li name="number" onClick={this._handleItemClick.bind(this)}>
-                    <div className={"collapsible-header"+ (this.state.type == "number" ? " active" : "")}>
-                        <i className="material-icons">whatshot</i>Number
+                    <div className="collapsible-body">
+                        <p>
+                            <BooleanValueComponent
+                                value={this.state.type == "boolean" ? this.state.value : false}
+                                feedback={this._handleComponentValueChange}/>
+                        </p>
                     </div>
-                    <div className="collapsible-body"><p><NumberValueComponent
-                        value={this.state.type == "number" ? this.state.value : 0}
-                        feedback={this._feedback.bind(this)}/>
-                    </p></div>
                 </li>
-                <li name="text" onClick={this._handleItemClick.bind(this)}>
-                    <div className={"collapsible-header"+ (this.state.type == "text" ? " active" : "")}>
+
+                <li name="number" onClick={this._handleComponentClick}>
+                    <div className={"collapsible-header" + (this.state.type == "number" ? " active" : "")}>
+                        <i className="material-icons">repeat_one</i>Number
+                    </div>
+                    <div className="collapsible-body">
+                        <p>
+                            <NumberValueComponent
+                                value={this.state.type == "number" ? this.state.value : 0}
+                                feedback={this._handleComponentValueChange}/>
+                        </p>
+                    </div>
+                </li>
+
+                <li name="text" onClick={this._handleComponentClick}>
+                    <div className={"collapsible-header" + (this.state.type == "text" ? " active" : "")}>
                         <i className="material-icons">spellcheck</i>Text
                     </div>
-                    <div className="collapsible-body"><p><TextValueComponent
-                        value={this.state.type == "text" ? this.state.value : ""} feedback={this._feedback.bind(this)}/>
-                    </p>
+                    <div className="collapsible-body">
+                        <p>
+                            <TextValueComponent
+                                value={this.state.type == "text" ? this.state.value : ""}
+                                feedback={this._handleComponentValueChange}/>
+                        </p>
                     </div>
                 </li>
+
                 <div className="container"><h5>Collection Types</h5></div>
-                <li name="array" onClick={this._handleItemClick.bind(this)}>
-                    <div className={"collapsible-header"+ (this.state.type == "array" ? " active" : "")}>
-                        <i className="material-icons">whatshot</i>Array
+
+                <li name="array" onClick={this._handleComponentClick}>
+                    <div className={"collapsible-header" + (this.state.type == "array" ? " active" : "")}>
+                        <i className="material-icons">reorder</i>Array
                     </div>
-                    <div className="collapsible-body"><p>This will add an empty array</p></div>
-                </li>
-                <li name="object" onClick={this._handleItemClick.bind(this)}>
-                    <div className={"collapsible-header"+ (this.state.type == "object" ? " active" : "")}>
-                        <i className="material-icons">whatshot</i>Object
+                    <div className="collapsible-body">
+                        <p>
+                            This will add an empty array
+                        </p>
                     </div>
-                    <div className="collapsible-body"><p>This will add an empty object</p></div>
                 </li>
+
+                <li name="object" onClick={this._handleComponentClick}>
+                    <div className={"collapsible-header" + (this.state.type == "object" ? " active" : "")}>
+                        <i className="material-icons">toc</i>Object
+                    </div>
+                    <div className="collapsible-body">
+                        <p>
+                            This will add an empty object
+                        </p>
+                    </div>
+                </li>
+
             </ul>
-            <button className="btn waves-effect">Okay</button>
+
+            <a className="btn waves-light waves-effect">Okay</a>
+            <a className="btn waves-light waves-effect">Cancel</a>
         </div>
     }
 
-    _handleItemClick(evt) {
-        if (this.state.type == evt) {
-            this.state.type = "";
-        }
-        this.state.type = evt;
+    _handleComponentClick(evt) { // synthetic event, -undefined, -event
+        // evt.persist();
+        // console.log(evt);
 
-        if (evt == "array") {
-            this.state.value = [];
-        } else if (evt == "object") {
-            this.state.value = {};
-        } else if (evt == "null") {
-            this.state.value = null;
+        let elem = evt.target;
+        do { // Find the list item, which has the name attribute containing type.
+            elem = elem.parentElement;
+        } while (elem.tagName != "LI");
+
+        let name = elem.attributes.getNamedItem('name').value;
+
+        if (name == this.state.type) {
+            elem.childNodes[0].classList.add("active");
+        }
+
+        console.log(this.state.value);
+        if (!this.state.value) { // A value that JavaScript accepts as false FIXME add more accurate logic
+            console.log('f');
+            switch (name) {
+                case "null":
+                    this.setState({type: "null", value: null});
+                    break;
+                case "boolean":
+                    this.setState({type: "boolean", value: false});
+                    break;
+                case "number":
+                    this.setState({type: "number", value: 0});
+                    break;
+                case "text":
+                    this.setState({type: "text", value: ""});
+                    break;
+                case "array":
+                    this.setState({type: "array", value: []});
+                    break;
+                case "object":
+                    this.setState({type: "object", value: {}});
+                    break;
+                default:
+                    break;
+            }
+            console.log(this.state);
+        } else {
+            console.log('t');
+            switch (name) {
+                case "null":
+                    this.setState({type: "null", value: null});
+                    break;
+                case "boolean":
+                    this.setState({type: "boolean", value: true});
+                    break;
+                case "number":
+                    this.setState({
+                        type: "number",
+                        value: Number.isFinite(this.state.value) ? Number(this.state.value) : 0
+                    });
+                    break;
+                case "text":
+                    this.setState({type: "text", value: String(this.state.value)});
+                    break;
+                case "array":
+                    this.setState({type: "array", value: []});
+                    // TODO Content preserving array to string back and forth conversion.
+                    break;
+                case "object":
+                    // TODO Object (de)serialization.
+                    this.setState({type: "object", value: {}});
+                    break;
+                default:
+                    break;
+            }
+            console.log(this.state);
         }
     }
 
-    _feedback(newObject) {
-        this.state.value = newObject.value;
+    _handleComponentValueChange(newObject) {
+        this.setState(Object.assign(this.state, {value: newObject.value}));
+        console.log(this.state);
     }
 }
+CombinedValueComponent.propTypes = {
+    value: React.PropTypes.oneOfType([
+        React.PropTypes.bool,
+        React.PropTypes.number,
+        React.PropTypes.string,
+        React.PropTypes.array,
+        React.PropTypes.object // or null
+    ]),
+    type: React.PropTypes.string
+};
+CombinedValueComponent.defaultProps = {value: null, type: "null"};
 
 
 class BreadcrumbComponent extends BaseComponent {
     constructor(props) {
         super(props);
+        this.state = {path: props.path};
+        this._handleNavigate = this._handleNavigate.bind(this);
     }
 
     render() {
         var items = [];
         this.state.path.forEach(item => items.push(
-            <a href="javascript:0" name={items.length} onClick={this._handleItemClick.bind(this)} className="breadcrumb"
+            <a href="javascript:(0)" name={items.length} onClick={this._handleNavigate} className="breadcrumb"
                key={items.length}>{item}</a>
         ));
         return <nav>
@@ -192,7 +310,7 @@ class BreadcrumbComponent extends BaseComponent {
         </nav>
     }
 
-    _handleItemClick(e) {
+    _handleNavigate(e) {
         var newPath = [];
         for (var i = 0; i < this.state.path.length; i++) {
             newPath.push(this.state.path[i]);
@@ -201,14 +319,17 @@ class BreadcrumbComponent extends BaseComponent {
             }
         }
         this.setState({path: newPath});
-        // TODO navigate to path
+        props.feedback({newPath});
     }
 }
+BreadcrumbComponent.propTypes = {path: React.PropTypes.array, feedback: React.PropTypes.func};
+BreadcrumbComponent.defaultProps = {path: ['[root]'], feedback: (obj) => console.log(obj.value)};
 
 
 class TreeComponent extends BaseComponent {
     constructor(props) {
         super(props);
+        this.state = {tree: props.tree}
     }
 
     render() {
@@ -259,7 +380,7 @@ class FullComponent extends BaseComponent {
         return <div>
             <BreadcrumbComponent path={this.props.path}/>
             <TreeComponent tree={this.props.data}/>
-            <CombinedValueComponent type="text" value={"asdfghjkl"}/>
+            <CombinedValueComponent type="null" value={null}/>
         </div>;
     }
 }
